60574f70855b0a02dfdd5d0d0802bdd36b4ba6ca
Thomas Newman <tntronic@gmail.com>
2017-06-19 21:20:35 -0400

    Wait for DB Init before clustering

---
+++ library/waitdbinit.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.urls import *
+
+KAZOO_DBS = ['accounts', 'acdc', 'alerts' , 'anonymous_cdrs', \
+        'dedicated_ips', 'faxes', 'global_provisioner', 'oauth', \
+        'offnet', 'port_requests', 'ratedeck', 'services', \
+        'sip_auth', 'system_auth', 'system_config', 'system_data', \
+        'system_media', 'system_schemas', 'tasks', 'token_auth', 'webhooks']
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(),
+        supports_check_mode = True
+    )
+
+    while True:
+        try:
+            response = open_url('http://localhost:15984/_all_dbs')
+            dbs = json.loads(response.read())
+        except Exception as ex:
+            module.fail_json(msg=str(ex))
+
+        if set(KAZOO_DBS).issubset(set(dbs)):
+            break
+
+    module.exit_json(changed=False)
+
+if __name__ == '__main__':
+    main()
+
