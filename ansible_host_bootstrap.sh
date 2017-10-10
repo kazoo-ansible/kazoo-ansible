d556416f6b23e6e3c493a51fd77f1f178fdf456b
Thomas Newman <tntronic@gmail.com>
2017-10-10 01:58:46 +0000

    Use correct SSH keypair path

--- ansible_host_bootstrap.sh
+++ ansible_host_bootstrap.sh
@@ -16,7 +16,14 @@ echo "Cloning kazoo-ansible Repository"
 git clone https://github.com/kazoo-ansible/kazoo-ansible ~/kazoo-ansible
 
 echo "Installing kazoo-ansible Roles"
-sudo ansible-galaxy install --force -r ~/kazoo-ansible/requirements.yml
+sudo ansible-galaxy install -r ~/kazoo-ansible/requirements.yml
+
+if [ -e "~/.ssh/id_rsa" -a -e "~/.ssh/id_rsa.pub" ]; then
+    echo "SSH keypair exists. Skipping..."
+else
+    echo "Generating SSH keypair"
+    ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+fi
 
 cd ~/kazoo-ansible
 
