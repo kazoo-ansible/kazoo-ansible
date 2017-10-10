5010842195997152d780f24f635090715e3d0470
Thomas Newman <tntronic@gmail.com>
2017-10-10 01:53:06 +0000

    Force latest role versions

--- ansible_host_bootstrap.sh
+++ ansible_host_bootstrap.sh
@@ -16,7 +16,7 @@ echo "Cloning kazoo-ansible Repository"
 git clone https://github.com/kazoo-ansible/kazoo-ansible ~/kazoo-ansible
 
 echo "Installing kazoo-ansible Roles"
-sudo ansible-galaxy install -r ~/kazoo-ansible/requirements.yml
+sudo ansible-galaxy install --force -r ~/kazoo-ansible/requirements.yml
 
 cd ~/kazoo-ansible
 
