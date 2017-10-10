58cc9c89d9663de39e43d88de1b4c4853416972c
Thomas Newman <tntronic@gmail.com>
2017-10-10 02:00:41 +0000

    Fix BASH syntax

--- ansible_host_bootstrap.sh
+++ ansible_host_bootstrap.sh
@@ -18,7 +18,7 @@ git clone https://github.com/kazoo-ansible/kazoo-ansible ~/kazoo-ansible
 echo "Installing kazoo-ansible Roles"
 sudo ansible-galaxy install -r ~/kazoo-ansible/requirements.yml
 
-if [ -e "~/.ssh/id_rsa" -a -e "~/.ssh/id_rsa.pub" ]; then
+if [[ -f ~/.ssh/id_rsa -a -f ~/.ssh/id_rsa.pub ]]; then
     echo "SSH keypair exists. Skipping..."
 else
     echo "Generating SSH keypair"
